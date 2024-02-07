9d21ddaac26c6dc2be090394a6a94b7c448fefd8
C-Dewar <133111661+C-Dewar@users.noreply.github.com>
2024-02-07 15:53:27 +0200

    IWA 7 & 8 Completed

---
+++ IWA7/Challenge 2/scripts.js
@@ -0,0 +1,11 @@
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+if(nickname === null && firstname!==null){
+const name = firstname
+console.log("Good Morning, ",name,"!")
+}else{
+    const name = nickname
+    console.log("Good Morning, ",name,"!")
+}
+
